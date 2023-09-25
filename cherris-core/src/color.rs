3ea77d24d8e944c396c43497fdac2c8c807fb653
Johannes Thiel <hi@johannesthiel.me>
2023-09-25 22:48:51 +0200

    added COUNT const to Color enum

--- cherris-core/src/color.rs
+++ cherris-core/src/color.rs
@@ -1,5 +1,20 @@
-/// Represents the two colors in chess.
+/// Represents the colors in chess.
 pub enum Color {
     White,
     Black,
+}
+
+impl Color {
+    /// The number of colors in chess.
+    pub const COUNT: usize = 2;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn const_count() {
+        assert_eq!(Color::COUNT, 2);
+    }
 }
