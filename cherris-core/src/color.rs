e08e80372054a66881f93186fb272e63cc51dda1
Johannes Thiel <hi@johannesthiel.me>
2023-09-25 22:56:45 +0200

    added to_index function to Color

--- cherris-core/src/color.rs
+++ cherris-core/src/color.rs
@@ -1,4 +1,5 @@
 /// Represents the colors in chess.
+#[derive(Clone, Copy)]
 pub enum Color {
     White,
     Black,
@@ -7,6 +8,11 @@ pub enum Color {
 impl Color {
     /// The number of colors in chess.
     pub const COUNT: usize = 2;
+
+    /// Converts the `Color` to a usize.
+    pub fn to_index(&self) -> usize {
+        *self as usize
+    }
 }
 
 #[cfg(test)]
@@ -16,5 +22,19 @@ mod tests {
     #[test]
     fn const_count() {
         assert_eq!(Color::COUNT, 2);
+    }
+
+    #[test]
+    fn to_index_white() {
+        let white = Color::White;
+
+        assert_eq!(white.to_index(), 0);
+    }
+
+    #[test]
+    fn to_index_black() {
+        let black = Color::Black;
+
+        assert_eq!(black.to_index(), 1);
     }
 }
