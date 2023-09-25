d49baca60d2f629b19135c519b3c254c33f851e5
Johannes Thiel <hi@johannesthiel.me>
2023-09-25 22:36:44 +0200

    added is_empty function to bitboard

--- cherris-core/src/bitboard.rs
+++ cherris-core/src/bitboard.rs
@@ -1,2 +1,28 @@
 /// A bitboard where each bit represents a square on a chess board.
 pub struct Bitboard(u64);
+
+impl Bitboard {
+    /// Determines whether the bitboard is empty or not.
+    pub fn is_empty(&self) -> bool {
+        self.0 == 0
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn is_empty() {
+        let bitboard = Bitboard(0);
+
+        assert_eq!(bitboard.is_empty(), true);
+    }
+
+    #[test]
+    fn is_not_empty() {
+        let bitboard = Bitboard(1);
+
+        assert_eq!(bitboard.is_empty(), false);
+    }
+}
